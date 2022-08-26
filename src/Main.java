0453f55a95d0ba5afe5a79cbc4aab89c3abe2052
Anna Shafeeva <dzenborg@gmail.com>
2022-08-26 12:52:20 +0300

    Private added

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    public static Map<Character, Integer> map = new HashMap<>();
-    public static String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+    private static Map<Character, Integer> map = new HashMap<>();
+    private static String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
             "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
             "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi " +
             "ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit " +
